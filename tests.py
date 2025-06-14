6da6dfa9520e1c2af876aba4ed26e348b153d9d0
davekznza <davekznza@users.noreply.github.com>
2025-06-14 14:05:28 +0200

    update test test_add_one

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from app import say_hello, add_one
 
 def test_say_hello():
-    assert say_hello("Alice") == "Hello, Alice!"
+    assert say_hello("Dave") == "Hello, Dave!"
 
 def test_add_one():
-    assert func(3) == 4
+    assert add_one(3) == 4
