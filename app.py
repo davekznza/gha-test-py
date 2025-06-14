893eeea872bd4adafffcf1055348a1650ddd16a6
davekznza <davekznza@users.noreply.github.com>
2025-06-14 15:14:40 +0200

    fix minor linting issues

--- app.py
+++ app.py
@@ -3,5 +3,6 @@
 def say_hello(name):
     return f"Hello, {name}!"
 
+
 def add_one(x):
     return x + 1
--- test_app.py
+++ test_app.py
@@ -1,10 +1,13 @@
 from app import say_hello, add_one
 
+
 def test_say_hello():
     assert say_hello("Dave") == "Hello, Dave!"
 
+
 def test_add_one():
     assert add_one(3) == 4
+
 
 print(say_hello("Dave"))
 print(add_one(3))
