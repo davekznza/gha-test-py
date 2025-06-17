9cbc096f47d8ae1e2b6f50dcf55ec79886794da9
davekznza <davekznza@users.noreply.github.com>
2025-06-17 20:44:10 +0200

    update tests

--- test_app.py
+++ test_app.py
@@ -1,13 +1,13 @@
-from app import say_hello, add_one
+from app import greet, add_one
 
 
-def test_say_hello():
-    assert say_hello("Dave") == "Hello, Dave!"
+def test_greet():
+    assert greet("Dave") == "Howsit, Dave!"
 
 
 def test_add_one():
     assert add_one(3) == 4
 
 
-print(say_hello("Dave"))
+print(greet("Dave"))
 print(add_one(3))
